beece2383eb776704527695b0366e8157269ed75
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2023-10-03 22:26:12 -0300

    Validação de senha forte.

--- SistemaContas.Presentations/Models/AccountRegisterModel.cs
+++ SistemaContas.Presentations/Models/AccountRegisterModel.cs
@@ -1,18 +1,24 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using SistemaContas.Presentations.Models.Validations;
+using System.ComponentModel.DataAnnotations;
 
 namespace SistemaContas.Presentations.Models
 {
     public class AccountRegisterModel
     {
+        [RegularExpression("^[A-Za-zà-üÀ-Ü\\s]{3,150}$", ErrorMessage = "Por favor, informe um nome válido.")]
+        [Required(ErrorMessage = "O nome é um campo obrigatório!")]
         public string? Name { get; set; }
 
-        [Required(ErrorMessage = "Informe seu E-mail.")]
-        [EmailAddress(ErrorMessage = "Informe um E-mail válido.")]
+        [Required(ErrorMessage = "O e-mail é um campo obrigatório!")]
+        [EmailAddress(ErrorMessage = "Por favor, informe um e-mail válido.")]
         public string? Email { get; set; }
 
-        [Required(ErrorMessage = "Informe sua Senha.")]
-        [MinLength(8, ErrorMessage = "A senha deve ter no mínimo {1} caractere.")]
-        [MaxLength(20, ErrorMessage = "A senha deve ter no máximo {1} caractere.")]
+        [StrongPassword(ErrorMessage = "Essa senha é muito fraca, querido.")]
+        [Required(ErrorMessage = "A senha é um campo obrigatório!")]
         public string? Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "As senhas são divergentes. Por favor, confirme sua senha.")]
+        [Required(ErrorMessage = "Por favor, confirme a sua senha!")]
+        public string? PasswordConfirmed { get; set; }
     }
 }
---
+++ SistemaContas.Presentations/Models/Validations/StrongPassword.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaContas.Presentations.Models.Validations
+{
+    /// <summary>
+    /// Validação de campo de senha forte.
+    /// </summary>
+    public class StrongPassword : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if(value != null)
+            {
+                //Convertendo o object para string.
+                var password = value.ToString();
+
+                //aplicando regras de validação.
+                return password.Length >= 8 && 
+                    password.Length <= 16 &&
+                    password.Any(char.IsUpper) &&
+                    password.Any(char.IsLower) &&
+                    password.Any(char.IsDigit) &&
+                    (
+                        password.Contains("@") ||
+                        password.Contains("!") ||
+                        password.Contains("#") ||
+                        password.Contains("$") ||
+                        password.Contains("%") ||
+                        password.Contains("&") ||
+                        password.Contains("*") ||
+                        password.Contains("+") ||
+                        password.Contains("-") 
+                     );
+
+            }
+
+            return false;
+        }
+    }
+}
