f179163dd3a08182a848a32a6069f6f37fadfaf9
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2023-11-27 23:38:37 -0300

    exclusão

--- SistemaContas.Presentations/Controllers/CategoryController.cs
+++ SistemaContas.Presentations/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SistemaContas.Data.Repositories;
@@ -24,7 +25,7 @@ namespace SistemaContas.Presentations.Controllers
         /// </summary>
         /// <param name="category"></param>
         /// <returns></returns>
-        [HttpPost] 
+        [HttpPost]
         public IActionResult Register(CategoryRegisterModel category)
         {
             //Todo método que recebe dados de um formulário é preciso verificar se esses dados são válidos
@@ -87,7 +88,7 @@ namespace SistemaContas.Presentations.Controllers
                 //e para cada uma criamos o obeto model, 
                 //no objeto model estamos preenchendo o id e nome da categoria.
                 //depois eu adiciono o objeto na listCategory, e envio o resultado para a página.
-                foreach(var category in categories)
+                foreach (var category in categories)
                 {
                     var model = new CategoryQueryModel();
                     model.Name = category.Name;
@@ -98,11 +99,69 @@ namespace SistemaContas.Presentations.Controllers
             }
             catch (Exception ex)
             {
-                TempData["MessageError"] = "Erro ao cadastrar categoria.";
+                TempData["MessageError"] = $"Erro ao cadastrar categoria: {ex}";
             }
 
             //Passando o objeto e enviando para a página.
             return View(listCategories);
+        }
+
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                var categoryRepository = new CategoryRepository();
+                var getId = categoryRepository.GetById(id);
+                var userModel = JsonConvert.DeserializeObject<UserModel>(User.Identity?.Name);
+                //verifica no banco se existe a categoria e se o usuário logado é o mesmo usuário da categoria. 
+                if (getId != null && getId.IdUser == userModel.IdUser)
+                {
+                    categoryRepository.Delete(getId);
+
+                    TempData["MessageSuccess"] = "Categoria deletada com sucesso";
+
+                }
+                else
+                {
+                    TempData["MessageAlert"] = "Algo deu errado! É provável que a categoria informada não exista no nosso banco. Verifique!";
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                TempData["MessageError"] = $"Erro ao Excluir categoria: {e}";
+
+            }
+
+
+            return RedirectToAction("Query");
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            try
+            {
+                var userModel = JsonConvert.DeserializeObject<UserModel>(User.Identity?.Name);
+                var categoryRepository = new CategoryRepository();
+                var getById = categoryRepository.GetById(id);
+
+                if (getById != null && userModel.IdUser == getById.IdUser)
+                {
+                    categoryRepository.Update(getById);
+                }
+                else
+                {
+                    TempData["MessageAlert"] = "Algo deu errado! É provável que a categoria informada não exista no nosso banco. Verifique!";
+
+                }
+            }
+            catch(Exception ex)
+            {
+                TempData["MessageError"] = $"Erro ao Editar categoria: {ex}";
+            }
+
+            return View("Register");
         }
     }
 }
