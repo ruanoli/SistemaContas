f2b4b36acee445db686445be6a536f5a6b675039
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2023-12-10 10:55:44 -0300

    carregamento da dropDownList de Categorias no cadastro de uma conta.

--- SistemaContas.Presentations/Controllers/BillController.cs
+++ SistemaContas.Presentations/Controllers/BillController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using SistemaContas.Data.Repositories;
+using SistemaContas.Presentations.Models;
 
 namespace SistemaContas.Presentations.Controllers
 {
@@ -7,12 +11,42 @@ namespace SistemaContas.Presentations.Controllers
     public class BillController : Controller
     {
         /// <summary>
-        /// Cadastro de contas
+        /// Além de abrir a página de cadastro de contas, é carregada as opções da combo CategoryList
         /// </summary>
         /// <returns></returns>
         public IActionResult Register()
         {
-            return View();
+            var billModel = new BillRegisterModel();
+
+            //inicializando a lista (separando um espaço na memória para armazenar as opções)
+            billModel.CategoryItems = new List<SelectListItem>();
+
+            try
+            {
+                var user = JsonConvert.DeserializeObject<UserModel>(User.Identity.Name);
+                var category = new CategoryRepository();
+                var categoryList = category.GetAllByUser(user.IdUser);
+
+                //indo ao banco percorrer as categorias já cadastradas e armazer o id e o nome na
+                //propriedade CategoryItems que é uma List<SelectListItem>, e por isso precisa de Value
+                // que é o id e Text que é o nome da categoria.
+                foreach (var item in categoryList)
+                {
+                    var selectListItems = new SelectListItem();
+                    selectListItems.Value = item.IdCategory.ToString();
+                    selectListItems.Text = item.Name;
+
+                    
+                    billModel.CategoryItems.Add(selectListItems);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                TempData["MessageError"] = "Não foi possível obter as categorias" + ex.Message;
+            }
+
+            return View(billModel);
         }
 
         /// <summary>
